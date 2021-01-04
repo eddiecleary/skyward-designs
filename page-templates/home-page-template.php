92e9ae8dba13c63fcd35c44507440302abd2c086
Eddie Cleary <eddie@eddiecleary.com>
2021-01-03 21:27:58 -0500

    first commit

---
+++ page-templates/home-page-template.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template Name: Home Page Template
+ *
+ * Template for displaying a the home page.
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+?>
+
+<main id="home">
+    <section class="hero">
+        <h1>hello world</h1>
+    </section>
+</main>
+
+<?php
+get_footer();
