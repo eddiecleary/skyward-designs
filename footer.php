f0dc4695203c8519292d3927fc433bb90ca46e75
Eddie Cleary <eddie@eddiecleary.com>
2021-01-06 16:59:23 -0500

    sizing bugs in footer and hero

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@ defined( 'ABSPATH' ) || exit;
         <div class="row d-flex flex-column flex-md-row align-items-center w-100 mx-auto">
             <?php the_custom_logo() ?>
             <?php wp_nav_menu(array(
-                'container_class'       => 'ml-md-auto col-lg-3 col-md-4 col-sm-5 col-6 d-flex align-items-center',
+                'container_class'       => 'ml-md-auto col-lg-3 col-md-4 col-sm-6 footer-nav d-flex align-items-center',
                 'menu_class'            => 'list-unstyled mt-5 mt-md-0 text-black d-flex justify-content-between w-100',
             )) ?>
             <p class="copyright col-12 text-center mt-4">Copyright &copy; <span class="year">2020</span> Skyward Designs</p>
