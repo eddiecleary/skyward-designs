5a82454aab0f112ff945009a2b064470cbcb927e
Eddie Cleary <eddie@eddiecleary.com>
2021-01-11 12:54:51 -0500

    added paragraphs to services page

--- page-templates/services-page-template.php
+++ page-templates/services-page-template.php
@@ -15,7 +15,7 @@ get_header();
 ?>
 
 <main id="services">
-    <section class="hero">
+    <section class="hero mt-3">
         <div class="container h-100">
             <div class="row h-100 justify-content-center align-items-center">
                 <div class="col text-center">
@@ -31,51 +31,51 @@ get_header();
                 <h2 class="col-12">Website Creation</h2>
                 <div class="col-xl-3 col-md-4 col-sm-6">
                     <div class="service-box d-flex flex-column">
-                        <img src="https://res.cloudinary.com/djoe/image/upload/v1610375265/Skyward%20Designs%20%28new%29/Icons/design_aerly0.svg" alt="Paint brush">
-                        <h3>Design</h3>
-                        <p>Web design is awesome. yes it is omg lol!!</p>
+                        <img class="w-25 mx-auto" src="https://res.cloudinary.com/djoe/image/upload/v1610375265/Skyward%20Designs%20%28new%29/Icons/design_aerly0.svg" alt="Paint brush">
+                        <h3 class="mt-4">Design</h3>
+                        <p>Beautiful, custom-designed websites to enhance your brand. Multiple revisions until it’s perfect.</p>
                     </div>
                 </div>
                 <div class="col-xl-3 col-md-4 col-sm-6">
                     <div class="service-box d-flex flex-column">
-                        <img src="https://res.cloudinary.com/djoe/image/upload/v1610375820/Skyward%20Designs%20%28new%29/Icons/development_rvczim.svg" alt="Paint brush">
-                        <h3>Development</h3>
-                        <p>Web design is awesome. yes it is omg lol!!</p>
+                        <img class="w-25 mx-auto" src="https://res.cloudinary.com/djoe/image/upload/v1610375820/Skyward%20Designs%20%28new%29/Icons/development_rvczim.svg" alt="Paint brush">
+                        <h3 class="mt-4">Development</h3>
+                        <p>Websites made with the latest and greatest technology. Built to look great and load fast on all devices.</p>
                     </div>
                 </div>
                 <div class="col-xl-3 col-md-4 col-sm-6">
                     <div class="service-box d-flex flex-column">
-                        <img src="https://res.cloudinary.com/djoe/image/upload/v1610375978/Skyward%20Designs%20%28new%29/Icons/e-commerce_s7dxmg.svg" alt="Paint brush">
-                        <h3>E-commerce</h3>
-                        <p>Web design is awesome. yes it is omg lol!!</p>
+                        <img class="w-25 mx-auto" src="https://res.cloudinary.com/djoe/image/upload/v1610375978/Skyward%20Designs%20%28new%29/Icons/e-commerce_s7dxmg.svg" alt="Paint brush">
+                        <h3 class="mt-4">E-Commerce</h3>
+                        <p>Easily add your books for sale on your website. Direct to consumer or affiliate links supported.</p>
                     </div>
                 </div>
                 <div class="col-xl-3 col-md-4 col-sm-6">
                     <div class="service-box d-flex flex-column">
-                        <img src="https://res.cloudinary.com/djoe/image/upload/v1610375978/Skyward%20Designs%20%28new%29/Icons/blog_tfqple.svg" alt="Paint brush">
-                        <h3>Blog</h3>
-                        <p>Web design is awesome. yes it is omg lol!!</p>
+                        <img class="w-25 mx-auto" src="https://res.cloudinary.com/djoe/image/upload/v1610375978/Skyward%20Designs%20%28new%29/Icons/blog_tfqple.svg" alt="Paint brush">
+                        <h3 class="mt-4">Blog</h3>
+                        <p>Start your online blog today! Easily write blog posts for your Amazon page or social media accounts.</p>
                     </div>
                 </div>
                 <div class="col-xl-3 col-md-4 col-sm-6">
                     <div class="service-box d-flex flex-column">
-                        <img src="https://res.cloudinary.com/djoe/image/upload/v1610375978/Skyward%20Designs%20%28new%29/Icons/hosting_xumksf.svg" alt="Paint brush">
-                        <h3>Hosting</h3>
-                        <p>Web design is awesome. yes it is omg lol!!</p>
+                        <img class="w-25 mx-auto" src="https://res.cloudinary.com/djoe/image/upload/v1610375978/Skyward%20Designs%20%28new%29/Icons/hosting_xumksf.svg" alt="Paint brush">
+                        <h3 class="mt-4">Hosting</h3>
+                        <p>Fast and reliable hosting to keep your website up and running. Only $99 a year and your first year free.</p>
                     </div>
                 </div>
                 <div class="col-xl-3 col-md-4 col-sm-6">
                     <div class="service-box d-flex flex-column">
-                        <img src="https://res.cloudinary.com/djoe/image/upload/v1610375981/Skyward%20Designs%20%28new%29/Icons/email_roepzw.svg" alt="Paint brush">
-                        <h3>Email</h3>
-                        <p>Web design is awesome. yes it is omg lol!!</p>
+                        <img class="w-25 mx-auto" src="https://res.cloudinary.com/djoe/image/upload/v1610375981/Skyward%20Designs%20%28new%29/Icons/email_roepzw.svg" alt="Paint brush">
+                        <h3 class="mt-4">Email</h3>
+                        <p>Custom e-mail address for your author website. Receive e-mails on your device wherever you go.</p>
                     </div>
                 </div>
                 <div class="col-xl-3 col-md-4 col-sm-6">
                     <div class="service-box d-flex flex-column">
-                        <img src="https://res.cloudinary.com/djoe/image/upload/v1610375981/Skyward%20Designs%20%28new%29/Icons/seo_mp8pqn.svg" alt="Paint brush">
-                        <h3>SEO</h3>
-                        <p>Web design is awesome. yes it is omg lol!!</p>
+                        <img class="w-25 mx-auto" src="https://res.cloudinary.com/djoe/image/upload/v1610375981/Skyward%20Designs%20%28new%29/Icons/seo_mp8pqn.svg" alt="Paint brush">
+                        <h3 class="mt-4">SEO</h3>
+                        <p>Your site is optimized with the latest SEO tricks to rank your site on search engines as fast as possible.</p>
                     </div>
                 </div>
             </div>
@@ -87,23 +87,23 @@ get_header();
                 <h2 class="col-12">Professional Reviews</h2>
                 <div class="col-xl-3 col-md-4 col-sm-6">
                     <div class="service-box d-flex flex-column">
-                        <img src="https://res.cloudinary.com/djoe/image/upload/v1610375979/Skyward%20Designs%20%28new%29/Icons/marketing_kxzhhj.svg" alt="Paint brush">
-                        <h3>Marketing</h3>
-                        <p>Web design is awesome. yes it is omg lol!!</p>
+                        <img class="w-25 mx-auto" src="https://res.cloudinary.com/djoe/image/upload/v1610375979/Skyward%20Designs%20%28new%29/Icons/marketing_kxzhhj.svg" alt="Paint brush">
+                        <h3 class="mt-4">Marketing</h3>
+                        <p>Your professional review is seen by 18,000+ loyal subscribers who can take interest in your book.</p>
                     </div>
                 </div>
                 <div class="col-xl-3 col-md-4 col-sm-6">
                     <div class="service-box d-flex flex-column">
-                        <img src="https://res.cloudinary.com/djoe/image/upload/v1610375979/Skyward%20Designs%20%28new%29/Icons/professionals_cco8gn.svg" alt="Paint brush">
-                        <h3>Professionals</h3>
-                        <p>Web design is awesome. yes it is omg lol!!</p>
+                        <img class="w-25 mx-auto" src="https://res.cloudinary.com/djoe/image/upload/v1610375979/Skyward%20Designs%20%28new%29/Icons/professionals_cco8gn.svg" alt="Paint brush">
+                        <h3 class="mt-4">Professionals</h3>
+                        <p>Your book is read in its entirety by a professional who will write an insightful and helpful review.</p>
                     </div>
                 </div>
                 <div class="col-xl-3 col-md-4 col-sm-6">
                     <div class="service-box d-flex flex-column">
-                        <img src="https://res.cloudinary.com/djoe/image/upload/v1610375978/Skyward%20Designs%20%28new%29/Icons/bookdescriptions_cytyzf.svg" alt="Paint brush">
-                        <h3>Book Descriptions</h3>
-                        <p>Web design is awesome. yes it is omg lol!!</p>
+                        <img class="w-25 mx-auto" src="https://res.cloudinary.com/djoe/image/upload/v1610375978/Skyward%20Designs%20%28new%29/Icons/bookdescriptions_cytyzf.svg" alt="Paint brush">
+                        <h3 class="mt-4">Descriptions</h3>
+                        <p>Your review can be used to compliment your book descriptions on Amazon or your social media.</p>
                     </div>
                 </div>
             </div>
@@ -115,28 +115,27 @@ get_header();
                 <h2 class="col-12">Video Trailers</h2>
                 <div class="col-xl-3 col-md-4 col-sm-6">
                     <div class="service-box d-flex flex-column">
-                        <img src="https://res.cloudinary.com/djoe/image/upload/v1610375981/Skyward%20Designs%20%28new%29/Icons/video_ixbwv5.svg" alt="Paint brush">
-                        <h3>High Quality Video</h3>
-                        <p>Web design is awesome. yes it is omg lol!!</p>
+                        <img class="w-25 mx-auto" src="https://res.cloudinary.com/djoe/image/upload/v1610375981/Skyward%20Designs%20%28new%29/Icons/video_ixbwv5.svg" alt="Paint brush">
+                        <h3 class="mt-4">Beautiful Video</h3>
+                        <p>Stunning HD quality video that will hook your audience and introduce them to the story in your book.</p>
                     </div>
                 </div>
                 <div class="col-xl-3 col-md-4 col-sm-6">
                     <div class="service-box d-flex flex-column">
-                        <img src="https://res.cloudinary.com/djoe/image/upload/v1610375981/Skyward%20Designs%20%28new%29/Icons/voiceover_nhgix7.svg" alt="Paint brush">
-                        <h3>Studio Voice Over</h3>
-                        <p>Web design is awesome. yes it is omg lol!!</p>
+                        <img class="w-25 mx-auto" src="https://res.cloudinary.com/djoe/image/upload/v1610375981/Skyward%20Designs%20%28new%29/Icons/voiceover_nhgix7.svg" alt="Paint brush">
+                        <h3 class="mt-4">Studio Voice</h3>
+                        <p>Studio quality voice overs recorded by a professional voice actor who reads your video script.</p>
                     </div>
                 </div>
                 <div class="col-xl-3 col-md-4 col-sm-6">
                     <div class="service-box d-flex flex-column">
-                        <img src="https://res.cloudinary.com/djoe/image/upload/v1610375981/Skyward%20Designs%20%28new%29/Icons/share_uruf26.svg" alt="Paint brush">
-                        <h3>Share on Social Media</h3>
-                        <p>Web design is awesome. yes it is omg lol!!</p>
+                        <img class="w-25 mx-auto" src="https://res.cloudinary.com/djoe/image/upload/v1610375981/Skyward%20Designs%20%28new%29/Icons/share_uruf26.svg" alt="Paint brush">
+                        <h3 class="mt-4">Share on Social</h3>
+                        <p>The best way to share your video to social media and entice your audience to read your book.</p>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 </main>
 
 <?php
